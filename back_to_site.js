10cdfe7d9033e23639eb87c99450d00db5d443da
mAtT <mattiabaraldo38@gmail.com>
2024-11-22 17:09:35 +0100

    Add files via upload

---
+++ back_to_site.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    // Aggiungi un evento di click al pulsante
+    document.getElementById("backToMain").addEventListener("click", function() {
+       // window.location.href = "https://gitginocchio.github.io/ScuolaAperta2024/"; // Cambia con il tuo sito principale
+        window.location.href = "index.html"; 
+    });
+});
